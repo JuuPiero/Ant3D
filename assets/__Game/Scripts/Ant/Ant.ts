50dd723d464ee4538c974168d1b0393ebd98c8a7
ThanhHH <hoangthanh100103@gmail.com>
2026-09-09 02:22:22 +0700

    update

--- assets/__Game/Scripts/Ant/Ant.ts
+++ assets/__Game/Scripts/Ant/Ant.ts
@@ -1,16 +1,24 @@
-import { _decorator, Component, Node, Renderer } from 'cc';
+import { _decorator, Color, Component, MeshRenderer, Node, Renderer } from 'cc';
 import { Tile } from '../Grid/Tile';
+import { ServiceLocator } from 'db://assets/_iKame/Scripts/ServiceLocator';
+import { ColorsConfig } from '../Data/ColorsConfig';
 const { ccclass, property } = _decorator;
 
 @ccclass('Ant')
 export class Ant extends Component {
     @property(Tile) target: Tile = null;
-    @property(Renderer) renderers: Renderer[] = [];
+    @property(MeshRenderer) renderers: MeshRenderer[] = [];
 
     setColor(colorId: number) {
+        const color = ServiceLocator.get(ColorsConfig).colors[colorId];
+        const shadowColor = new Color(color.r * 0.6, color.g * 0.6, color.b * 0.6, color.a);
 
+        for (const renderer of this.renderers) {
+            renderer.setInstancedAttribute('a_instMainColor', [color.r, color.g, color.b, color.a]);
+            renderer.setInstancedAttribute('a_instShadowColor', [shadowColor.r, shadowColor.g, shadowColor.b, shadowColor.a]);
+        }
     }
-   
+
 }
 
 
