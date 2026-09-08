a465f6a966fc6de4b96e9fb3736923b2d6cd7fbc
ThanhHH <thanhhh@ikameglobal.com>
2026-09-08 18:44:16 +0700

    update

--- assets/__Game/Scripts/GameManager.ts
+++ assets/__Game/Scripts/GameManager.ts
@@ -35,14 +35,16 @@ export class GameManager extends Component {
         EventBus.on(GameEvents.NEW_LEVEL, this.onNewgame);
         EventBus.on(GameEvents.LEVEL_WIN, this.onWinGame);
         EventBus.on(GameEvents.LEVEL_LOSE, this.onLoseGame);
-
+        EventBus.on(GameEvents.TOGGLE_VIDEO, this.onToggleVideo);
 
     }
 
     protected onDisable(): void {
         EventBus.off(GameEvents.NEW_LEVEL, this.onNewgame)
         EventBus.off(GameEvents.LEVEL_WIN, this.onWinGame)
-        EventBus.off(GameEvents.LEVEL_LOSE, this.onLoseGame)
+        EventBus.off(GameEvents.LEVEL_LOSE, this.onLoseGame);
+        EventBus.off(GameEvents.TOGGLE_VIDEO, this.onToggleVideo);
+
     }
 
 
